f13f3defe88c06ba1f439f7d0747f19b6334f613
Nicolas Deslandes <ndeslandes@live.fr>
2017-12-06 09:12:09 +0000

    Added -v and -ssl options

--- TcpMux/Program.cs
+++ TcpMux/Program.cs
@@ -14,6 +14,9 @@ namespace TcpMux
 {
     class Program
     {
+        static bool Verbose = false;
+        static bool Ssl = false;
+
         static readonly RemoteCertificateValidationCallback ServerCertificateValidationCallback =
             (object sender, X509Certificate cert, X509Chain chain, SslPolicyErrors sslPolicyErrors) =>
         {
@@ -23,16 +26,45 @@ namespace TcpMux
         };
         static int Main(string[] args)
         {
-            if (args.Length != 3)
+            if (args.Length < 3)
             {
-                Console.Error.WriteLine($"Usage: tcpmux <listen_port> <target_host> <target_port>");
+                Console.Error.WriteLine($"Usage: tcpmux [options] <listen_port> <target_host> <target_port>\n" +
+                                        $"  options:\n" +
+                                        $"     -v: Verbose mode; display traffic\n" +
+                                        $"     -ssl: perform ssl decoding and reencoding\n\n"
+                    );
                 return 1;
+            }
+
+            int i = 0;
+            while (true)
+            {
+                if (args[i][0] == '-')
+                {
+                    var option = args[i++];
+                    switch (option)
+                    {
+
+                        case "-v":
+                            Verbose = true;
+                            break;
+                        case "-ssl":
+                            Ssl = true;
+                            break;
+                        default:
+                            Console.WriteLine($"Invalid option: {option}");
+                            return 1;
+                    }
+                }
+                else
+                {
+                    break;
+                }
             }
 
             int listenPort = int.Parse(args[0]);
             string targetHost = args[1];
             int targetPort = int.Parse(args[2]);
-
 
             Console.WriteLine($"Preparing message routing to {targetHost}:{targetPort}");
             Console.Write($"Opening local port {listenPort}...");
@@ -41,9 +73,14 @@ namespace TcpMux
             listener.Start();
             Console.WriteLine(" done");
 
-            var store = new X509Store(StoreLocation.CurrentUser);
-            store.Open(OpenFlags.OpenExistingOnly);
-            var certificate = store.Certificates.Cast<X509Certificate2>().First(c => c.Subject == "CN=surface4");
+            X509Certificate2 certificate = null;
+            if (Ssl)
+            {
+                var store = new X509Store(StoreLocation.CurrentUser);
+                store.Open(OpenFlags.OpenExistingOnly);
+                certificate = store.Certificates.Cast<X509Certificate2>().First(c => c.Subject == "CN=surface4");
+            }
+
             Task.Run(async () =>
             {
                 while (true)
@@ -72,17 +109,24 @@ namespace TcpMux
                 var target = new TcpClient(targetHost, targetPort) {NoDelay = true};
                 Console.WriteLine($" opened target connection: {target.Client.RemoteEndPoint}");
 
-                var sourceStream = new SslStream(client.GetStream());
-                Console.WriteLine($"Performing SSL authentication with client {client.Client.RemoteEndPoint}");
-                await sourceStream.AuthenticateAsServerAsync(certificate, false,
-                    SslProtocols.Tls | SslProtocols.Tls11 | SslProtocols.Tls12, false);
-                Console.WriteLine($"SSL authentication with client {client.Client.RemoteEndPoint} successful");
+                Stream sourceStream = client.GetStream();
+                Stream targetStream = target.GetStream();
 
-                Console.WriteLine($"Performing SSL authentication with server {target.Client.RemoteEndPoint}");
-                var targetStream = new SslStream(target.GetStream(), false, ServerCertificateValidationCallback);
-                await targetStream.AuthenticateAsClientAsync(targetHost, null, SslProtocols.Tls12, false);
-                Console.WriteLine($"SSL authentication with server {target.Client.RemoteEndPoint} successful; " +
-                                  $"server cert Subject: {targetStream.RemoteCertificate?.Subject}");
+                if (Ssl)
+                {
+                    var sslSourceStream = new SslStream(client.GetStream());
+                    Console.WriteLine($"Performing SSL authentication with client {client.Client.RemoteEndPoint}");
+                    await sslSourceStream.AuthenticateAsServerAsync(certificate, false,
+                        SslProtocols.Tls | SslProtocols.Tls11 | SslProtocols.Tls12, false);
+                    Console.WriteLine($"SSL authentication with client {client.Client.RemoteEndPoint} successful");
+                    sourceStream = sslSourceStream;
+
+                    Console.WriteLine($"Performing SSL authentication with server {target.Client.RemoteEndPoint}");
+                    var sslTargetStream = new SslStream(targetStream, false, ServerCertificateValidationCallback);
+                    await sslTargetStream.AuthenticateAsClientAsync(targetHost, null, SslProtocols.Tls12, false);
+                    Console.WriteLine($"SSL authentication with server {target.Client.RemoteEndPoint} successful; " +
+                                      $"server cert Subject: {sslTargetStream.RemoteCertificate?.Subject}");
+                }
 
                 RouteMessages(sourceStream, client.Client.RemoteEndPoint,
                     targetStream, target.Client.RemoteEndPoint);
@@ -116,7 +160,8 @@ namespace TcpMux
                         $"Sending data from {sourceRemoteEndPoint} to {targetRemoteEndPoint}...");
                     await target.WriteAsync(buffer, 0, read);
                     Console.WriteLine($" {read} bytes sent");
-                    Console.WriteLine(Utils.HexDump(buffer, 0, read));
+                    if (Verbose)
+                        Console.WriteLine(Utils.HexDump(buffer, 0, read));
                 }
             });
         }
